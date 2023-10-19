3171819fd935ead191b6267f48968d52023a8d8f
sarim nadeem <115877996+samcodeonline@users.noreply.github.com>
2023-10-19 18:04:20 +0500

    react

---
+++ Components/Header.js
@@ -0,0 +1,52 @@
+// "use client";
+// import React, { useState } from 'react';
+
+// const Header = (props) => {
+//     const [name, setname] = useState("sarim nadeem");
+//   return (
+//     <>
+//         <p style={{fontWeight:"bold"}}>my full name is {props.webdev}</p>
+//         <div onClick={()=>{
+//             setname("rahul");
+//         }} style={{cursor:'pointer'}}>
+//             you are the one i was <span style={{fontWeight:'bolder'}}>{name} </span> 
+//         </div>
+//     </>
+//   )
+// }
+
+// export default Header
+
+// "use client"
+// import React from 'react'
+// import Link from 'next/link'
+// const Header = () => {
+//   return (
+//     <>
+//     <div className='navbar'>
+//     <Link className='navItems' href="/About">
+//       About
+//     </Link>
+//     <Link className='navItems' href="/Contact">
+//       Contact
+//     </Link>
+//     <Link className='navItems' href="/Courses">
+//       Courses
+//     </Link>
+      
+//     </div>
+//     </>
+//   )
+// }
+
+// export default Header
+"use client"
+import React from 'react'
+
+const Header = () => {
+  return (
+    <div>Header</div>
+  )
+}
+
+export default Header
